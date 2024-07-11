63bd3e2c26a8ff0a812282ee24945f79e52cb68d
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-07-10 22:40:30 -0500

    h

--- src/client/Modules/Env.lua
+++ src/client/Modules/Env.lua
@@ -204,7 +204,7 @@ return function(C,Settings)
 
 	function C.GetPlayerNameTagColor(theirPlr,theirChar)
 		if theirPlr.Team then
-			return theirPlr.Team.TeamColor.Color3
+			return theirPlr.Team.TeamColor.Color
 		else
 			return Color3.fromRGB(0,0,255)
 		end
