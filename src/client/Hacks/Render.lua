052153ba5c289503b81eac5db561f446e06797ff
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-07-10 23:19:29 -0500

    h

--- src/client/Hacks/Render.lua
+++ src/client/Hacks/Render.lua
@@ -14,11 +14,44 @@ return function(C,Settings)
 			{
 				Title = "ESP Player Highlight",
 				Tooltip = "Highlights users' characters when they are not visible on the screen",
-				Layout = 1,Default=true,
-				Shortcut = "PlayerHighlight", Instances = {},
+				Layout = 1,Default=true,Deb=0,
+				Shortcut = "PlayerHighlight", Instances = {}, Storage={},
+				UpdVisibility = function(self,robloxHighlight,enabled,color)
+					robloxHighlight.FillTransparency = enabled and 0 or 1
+					robloxHighlight.OutlineTransparency = enabled and 0 or 1
+					if color then
+						robloxHighlight.FillColor = color
+					end
+				end,
+				checkIfInRange = function(self,camera,theirPlr,theirChar,HRP)
+					if self.EnTbl.Distance < 0.1 then
+						return false -- no way we're reaching them lol!
+					end							
+					local options = {
+						ignoreInvisibleWalls = self.EnTbl.IgnoreInvisibleWalls,
+						ignoreUncollidable = self.EnTbl.IgnoreUncollidibleWalls,
+						ignoreList = {camera.CameraSubject and camera.CameraSubject.Parent or nil},  -- Example: ignore parts in this list
+						raycastFilterType = Enum.RaycastFilterType.Exclude,  -- Choose filter type
+						distance = self.EnTbl.Distance, -- Maximum cast distance
+						detectionFunction = function(part)
+							return theirChar:IsAncestorOf(part)--part:HasTag("CharPart") and 
+						end,
+						passFunction = function(part)
+							return part:HasTag("CharPart")
+						end,
+					}
+
+					local hitResult, hitPosition = C.Raycast(camera.CFrame.Position,(HRP.Position - camera.CFrame.Position).Unit,options)
+					return hitResult and theirChar:IsAncestorOf(hitResult.Instance)
+				end,
 				Activate = function(self,newValue)
+					self.Storage = {}
 					if not newValue then
 						return
+					end
+					self.Deb = self.Deb + 1 local saveDeb = self.Deb
+					local function CanRun()
+						return saveDeb == self.Deb and not C.Cleared
 					end
 					for num, theirPlr in ipairs(PS:GetPlayers()) do
 						local theirChar = theirPlr.Character
@@ -26,61 +59,33 @@ return function(C,Settings)
 							task.spawn(self.Events.CharAdded,self,theirPlr,theirChar)
 						end
 					end
+					local camera = workspace.CurrentCamera
+					while CanRun() do
+						for num, instanceData in ipairs(self.Storage) do
+							local theirPlr,theirChar,robloxHighlight,theirHumanoid,HRP = table.unpack(instanceData)
+							if theirHumanoid~=camera.CameraSubject and (not C.isInGame or (({C.isInGame(theirChar)})[1])==({C.isInGame(camera.CameraSubject.Parent)})[1]) then
+								local isInRange = self:checkIfInRange(camera,theirPlr,theirChar,HRP)
+								self:UpdVisibility(robloxHighlight,not isInRange, C.GetPlayerNameTagColor(theirPlr,theirChar))
+							else
+								self:UpdVisibility(robloxHighlight,false)
+							end
+						end
+						task.wait(self.EnTbl.UpdateTime)
+					end
 				end,
 				Events = {
 					CharAdded = function(self,theirPlr,theirChar,firstRun)
 						RunService.RenderStepped:Wait()
-						print("CHARADD")
 						if firstRun or C.Cleared then
 							return
 						end
-						local camera = workspace.CurrentCamera
 						local robloxHighlight = Instance.new("Highlight")
 						robloxHighlight.Parent = theirChar
 						robloxHighlight.OutlineColor = Color3.fromRGB()
 						table.insert(self.Instances,robloxHighlight)
 						local theirHumanoid = theirChar:WaitForChild("Humanoid",1000)
 						local HRP = theirChar:WaitForChild("HumanoidRootPart")
-						local function changeVisibility(enabled,color)
-							robloxHighlight.FillTransparency = enabled and 0 or 1
-							robloxHighlight.OutlineTransparency = enabled and 0 or 1
-							if color then
-								robloxHighlight.FillColor = color
-							end
-						end
-						local function checkIfInRange()
-							if self.EnTbl.Distance < 0.1 then
-								return false -- no way we're reaching them lol!
-							end							
-							local options = {
-								ignoreInvisibleWalls = self.EnTbl.IgnoreInvisibleWalls,
-								ignoreUncollidable = self.EnTbl.IgnoreUncollidibleWalls,
-								ignoreList = {camera.CameraSubject and camera.CameraSubject.Parent or nil},  -- Example: ignore parts in this list
-								raycastFilterType = Enum.RaycastFilterType.Exclude,  -- Choose filter type
-								distance = self.EnTbl.Distance, -- Maximum cast distance
-								detectionFunction = function(part)
-									return theirChar:IsAncestorOf(part)--part:HasTag("CharPart") and 
-								end,
-								passFunction = function(part)
-									return part:HasTag("CharPart")
-								end,
-							}
-		
-							local hitResult, hitPosition = C.Raycast(camera.CFrame.Position,(HRP.Position - camera.CFrame.Position).Unit,options)
-							return hitResult and theirChar:IsAncestorOf(hitResult.Instance)
-						end
-						while self.EnTbl.En and HRP and not C.Cleared do
-							if (not C.isInGame or (({C.isInGame(theirChar)})[1])==({C.isInGame(camera.CameraSubject.Parent)})[1]) then
-								changeVisibility(not checkIfInRange(), C.GetPlayerNameTagColor(theirPlr,theirChar))
-							else
-								changeVisibility(false)
-							end
-							while theirHumanoid==camera.CameraSubject do
-								changeVisibility(false)
-								camera:GetPropertyChangedSignal("CameraSubject"):Wait()
-							end
-							task.wait(self.EnTbl.UpdateTime)
-						end
+						table.insert(self.Storage,{theirPlr,theirChar,robloxHighlight,theirHumanoid,HRP})
 					end,
 				},
 				Options = {
