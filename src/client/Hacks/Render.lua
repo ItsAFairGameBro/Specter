a2f886af43e76d6c9c1340cb21cc2e2ff6b51bb4
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-05 23:03:04 -0500

    update Render.lua

--- src/client/Hacks/Render.lua
+++ src/client/Hacks/Render.lua
@@ -54,7 +54,7 @@ return function(C,Settings)
 					local camera = workspace.CurrentCamera
 					local theirPlr,theirChar,robloxHighlight,theirHumanoid,HRP = table.unpack(instanceData)
 					local theirInGame = C.isInGame and table.pack(C.isInGame(theirChar))
-					print(theirInGame[3],C.isInGame(camera.CameraSubject.Parent),table.pack(C.isInGame(camera.CameraSubject.Parent))[3])
+					print(theirInGame[3],theirInGame[3]~=nil,C.isinGame(camera.CameraSubject.Parent),(theirInGame[3]~=nil and C.isinGame(camera.CameraSubject.Parent)==theirInGame[1]))
 					local shouldRender = theirHumanoid~=camera.CameraSubject and (not theirInGame or
 					((theirInGame[3]==nil and table.pack(C.isInGame(camera.CameraSubject.Parent))[3]==theirInGame[3]) or
 					(theirInGame[3]~=nil and C.isinGame(camera.CameraSubject.Parent)==theirInGame[1])))
