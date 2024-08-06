38dace3ba8bc8428159c3f753c334108951e7f1d
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-06 17:23:23 -0500

    update MurderMystery.lua

--- src/client/Games/MurderMystery.lua
+++ src/client/Games/MurderMystery.lua
@@ -360,8 +360,8 @@ return function(C,Settings)
                         local inviPart = Instance.new("Part")
                         inviPart.TopSurface = Enum.SurfaceType.Smooth
                         inviPart.BottomSurface = Enum.SurfaceType.Smooth
-                        inviPart.Color = Color3.fromRGB(255,255,255)
-                        inviPart.CFrame = cf * CFrame.new(0,-size.Y/2,0)
+                        inviPart.Color = Color3.fromRGB()
+                        inviPart.CFrame = cf + Vector3.new(0,-size.Y/2,0)
                         inviPart.Size = Vector3.new(size.X,0.2,size.Z) + 120 * 2 * Vector3.new(1,0,1)
                         inviPart.Anchored = true
                         inviPart.Parent = map
