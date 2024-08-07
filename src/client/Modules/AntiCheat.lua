5eb48426b8b749bdb8dde032dc3be19131443e40
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-06 22:06:04 -0500

    update AntiCheat.lua and init.client.lua update init.client.lua

--- src/client/Modules/AntiCheat.lua
+++ src/client/Modules/AntiCheat.lua
@@ -23,10 +23,26 @@ return function(C,Settings)
             end,
             KeepGoing = false, RunOnce = true,
             GameIds = {1069466626},
+        },
+        {
+            Run = function(self)
+                local Old
+                Old = C.hookfunction(getrenv().game:GetService("ContentProvider").PreloadAsync, function(funct,...)
+                    warn("Preloaded from",C.getcallingscript())
+                    if not C.checkcaller() then
+                        return yieldForeverFunct()
+                    end
+                    return Old(funct,...)
+                end)
+                return true -- indicate that the anti cheat is successful
+            end,
+            KeepGoing = false, RunOnce = true,
+            GameIds = {},
+            PlaceIds = {352947107},
         }
     }
     for num, cheatTbl in ipairs(AntiCheat) do
-        if table.find(cheatTbl.GameIds,game.GameId) then
+        if table.find(cheatTbl.GameIds,game.GameId) or table.find(cheatTbl.PlaceIds,game.PlaceId) then
             if not cheatTbl.RunOnce or not C.getgenv()["AntiCheat"..num] then
                 C.DebugMessage("AntiCheat",`AntiCheat {num} Hooked`)
                 cheatTbl.Run(cheatTbl)
