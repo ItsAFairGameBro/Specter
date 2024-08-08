ec00bef210420f58a3e085b4a502588f9584d6a9
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-08 10:39:59 -0500

    update CommandCore.lua and Env.lua

--- src/client/Modules/Env.lua
+++ src/client/Modules/Env.lua
@@ -68,22 +68,22 @@ return function(C,Settings)
 			return str
 		end
 		local DoPrefix = false
-		OldEnv.print1 = C.hookfunction(getrenv().print,function(...)
+		OldEnv.print1 = C.hookfunction(C.getrenv().print,function(...)
 			if C.checkcaller() then
 				return OldEnv.print1(...)
 			end
 			return OldEnv.print1(`{DoPrefix and "[GAME]: " or ""}` .. recurseLoopPrint({...}))
 		end)
-		OldEnv.warn1 = C.hookfunction(getrenv().warn, function(...)
+		OldEnv.warn1 = C.hookfunction(C.getrenv().warn, function(...)
 			if C.checkcaller() then
 				return OldEnv.warn1(...)
 			end
 			return OldEnv.warn1(`{DoPrefix and "[GAME]: " or ""}` .. recurseLoopPrint({...}))
 		end)
-		OldEnv.print2 = C.hookfunction(getgenv().print,function(...)
+		OldEnv.print2 = C.hookfunction(C.getgenv().print,function(...)
 			return OldEnv.print2(`{DoPrefix and "[HACK]: " or ""}` .. recurseLoopPrint({...}))
 		end)
-		OldEnv.warn2 = C.hookfunction(getgenv().warn, function(...)
+		OldEnv.warn2 = C.hookfunction(C.getgenv().warn, function(...)
 			return OldEnv.warn2(`{DoPrefix and "[HACK]: " or ""}` .. recurseLoopPrint({...}))
 		end)
 		
