498f722a1ce6ee6dd8fd367874d935a641ae0430
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-10 17:26:14 -0500

    update AntiCheat.lua

--- src/client/Modules/AntiCheat.lua
+++ src/client/Modules/AntiCheat.lua
@@ -96,11 +96,11 @@ return function(C,Settings)
         {
             Run = function(self)
                 C.HookNamecall("AntiCheat5",{"fireserver","invokeserver"},function(theirScript,method,self,arg1,...)
-                if (not theirScript.Parent or theirScript.Name == "BAC_") and (typeof(arg1) ~= "table" or arg1 ~= 0) then--(arg1[1]~=arg1)) then --and arg1 ~= 0 then
+                if (not theirScript.Parent or theirScript.Name == "BAC_") and (typeof(arg1) ~= "table" and (arg1[1]~=arg1)) and arg1 ~= 0 then--(arg1[1]~=arg1)) then --and arg1 ~= 0 then
                         if typeof(arg1) == "table" then
-                            C.DebugMessage("AntiCheat",`CANCELLING ON: {theirScript:GetFullName()} because it tried sending method {self.Name} with table arg1 index 1={tostring(arg1[1])}`)
+                            --C.DebugMessage("AntiCheat",`CANCELLING ON: {theirScript:GetFullName()} because it tried sending method {self.Name} with table arg1 index 1={tostring(arg1[1])}`)
                         else
-                            C.DebugMessage("AntiCheat",`CANCELLING ON: {theirScript:GetFullName()} because it tried sending method {self.Name} with arg1 {tostring(arg1)}`)
+                            --C.DebugMessage("AntiCheat",`CANCELLING ON: {theirScript:GetFullName()} because it tried sending method {self.Name} with arg1 {tostring(arg1)}`)
                         end
                         return "Cancel"
                     elseif theirScript.Name == "BAC_" then
@@ -109,7 +109,7 @@ return function(C,Settings)
                                 --print("Pass",self,arg)
                          --   end
                         --end
-                        --print("YIELDING")
+                        print("CANCELLING TWO WITH #",#table.pack(arg1,...))
                         local args = {
                             [1] = "A-1",
                             [2] = "RSSERV MTHD"
