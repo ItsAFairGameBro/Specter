8915a88e8e42c4d6c87c07df57e210a095dbb60a
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-10 08:51:48 -0500

    update AntiCheat.lua and BlockRemotes.lua

--- src/utility/BlockRemotes.lua
+++ src/utility/BlockRemotes.lua
@@ -1,15 +1,22 @@
--- Store the original remote functions
---[[local originalFireServer = Instance.new("RemoteEvent").FireServer
-local originalInvokeServer = Instance.new("RemoteFunction").InvokeServer
+local TheRemoteEvent = game:GetService("ReplicatedStorage"):FindFirstChildWhichIsA("RemoteEvent",true)
 
 local function yieldForeverFunct()
-    game:WaitForChild("PeePeePooPoo")
+    print("Yielding Forever Funct!")
+    game:WaitForChild("PeePeePooPoo",math.huge)
 end
+
+--[[
+-- Store the original remote functions
+local originalFireServer = TheRemoteEvent.FireServer
+local originalInvokeServer = Instance.new("RemoteFunction").InvokeServer
+
+
 
 -- Hook the functions to block remotes
 hookfunction(originalFireServer, function(...)
+    print("FIRED")
     -- If the caller is not from the local script, block the request
-    if not checkcaller() then
+    if not checkcaller() or true then
         warn("RemoteFunction:FireServer: "..getcallingscript():GetFullName().." with args:",...)
         return yieldForeverFunct()
     end
@@ -20,7 +27,7 @@ end)
 
 hookfunction(originalInvokeServer, function(...)
     -- If the caller is not from the local script, block the request
-    if not checkcaller() then
+    if not checkcaller() or true then
         warn("RemoteFunction:InvokeServer: "..getcallingscript():GetFullName().." with args:",...)
         return yieldForeverFunct()
     end
@@ -30,29 +37,73 @@ hookfunction(originalInvokeServer, function(...)
 end)
 
 -- Notify that the protection is active
-print("Remote blocking is active.")
 --]]
 
 -- Store the original namecall method
 local originalNamecall = nil
+local getgenv = getgenv
 
--- Hook the namecall function
-originalNamecall = hookmetamethod(game, "__namecall", function(self, ...)
-    -- Check if the caller is not a local script
-    if not checkcaller() or true then
-        -- Get the method being called
-        local method = getnamecallmethod()
+local function HookNamecall(name,methods,runFunct)
+    if not getgenv().NamecallHooks then
+        getgenv().NamecallHooks = {}
+        -- Hook the namecall function
+        local getcallingscript,getnamecallmethod,lower,tblFind,tblPack,tblUnpack = getcallingscript,getnamecallmethod,string.lower,table.find,table.pack,table.unpack
+        originalNamecall = hookmetamethod(game, "__namecall", function(self, ...)
+            -- Check if the caller is not a local script
+            if not checkcaller() and self.Name ~= "CharacterSoundEvent" then
+                -- Get the method being called
+                local method = lower(getnamecallmethod())
+                local theirScript = getcallingscript()
+                -- Block FireServer or InvokeServer methods
+                for name, list in pairs(getgenv().NamecallHooks) do
+                    if tblFind(list[2],method) then -- Authorization
+                        local operation,returnData = runFunct(theirScript,method,self,...)
+                        if operation then
+                            if operation == "Override" then
+                                print("Overridden")
+                                return tblUnpack(returnData)
+                            elseif operation == "Cancel" then
+                                print("Cancelled!")
+                                return
+                            elseif operation == "Yield" then
+                                return yieldForeverFunct()
+                            else
+                                warn(`[C.HookNameCall]: Unknown Operation for {name}: {operation}`)
+                            end
+                        end
+                        
+                        warn(`Blocked a {method} from {self.Name}: {theirScript}. Args:`,...)
+                    end
+                end
+            end
 
-        -- Block FireServer or InvokeServer methods
-        if method == "FireServer" or method == "InvokeServer" then
-            warn(`Blocked a remote call attempt from an external script: {getcallingscript()}. Args:`,...)
-            return -- Prevent the remote call from being executed
-        end
+            -- If the caller is a local script, call the original namecall method
+            return originalNamecall(self, ...)
+        end)--]]
     end
+    if runFunct or #methods==0 then
+        getgenv().NamecallHooks[name] = {runFunct,methods}
+    else
+        getgenv().NamecallHooks[name] = nil
+    end
+end
 
-    -- If the caller is a local script, call the original namecall method
-    return originalNamecall(self, ...)
+HookNamecall("AntiRemotes",{"fireserver","invokeserver"},function(theirScript,method,self,...)
+    return "Cancel"
 end)
-
 -- Notify that the protection is active
-print("Namecall hook to block remote calls is active.")
+print("✅✅Namecall hook to block remote calls is active.")
+--task.wait(1)
+--print("Activating Test Call",TheRemoteEvent)
+--task.wait(1)
+TheRemoteEvent:FireServer("Hi Bob")
+
+
+--task.cancel(getconnections(game.LogService.MessageOut)[2].Thread)
+
+--[[
+
+
+for num, part in ipairs(workspace:GetDescendants()) do if part:IsA("BasePart") then part.CanQuery = false end end
+
+]]
