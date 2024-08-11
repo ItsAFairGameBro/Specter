81abd4da46bed79c96a70e580b200b7912ac405f
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-11 14:28:07 -0500

    update FlagWars.lua and Env.lua

--- src/client/Games/FlagWars.lua
+++ src/client/Games/FlagWars.lua
@@ -52,7 +52,6 @@ return function (C,Settings)
                             return
                         end
                         if self.RealEnabled then
-                            print("Set",name)
                             C.SetPartProperty(val,"Value",self.Shortcut,newValue,true)
                         else
                             C.ResetPartProperty(val,"Value",self.Shortcut)
