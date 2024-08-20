8de9849d986c4a2430602546872e5bb7a92217a1
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-20 18:14:50 -0500

    update init.client.lua

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -248,7 +248,7 @@ C.BindedActions = {} -- key binds
 C.EventFunctions = {}
 C.Camera = workspace.CurrentCamera -- updated later in Events
 C.Randomizer = Random.new()
-C.Debugs = {All = false,
+C.Debugs = {All = true,
 	Destroy = false,
 	Module = false,
 	Load = false,
