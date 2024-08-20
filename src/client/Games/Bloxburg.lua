61b68e7a8ef763e3c1bb150c083c3c64ed04e8e9
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-20 17:03:37 -0500

    ww

---
+++ src/client/Games/Bloxburg.lua
@@ -0,0 +1,55 @@
+local Types = {Toggle="Toggle",Slider="Slider",Dropdown="Dropdown",UserList="UserList"}
+
+local Players = game:GetService("Players")
+local RunS = game:GetService("RunService")
+local RS = game:GetService("ReplicatedStorage")
+local function Static(C,Settings)
+    
+end
+
+return function (C,Settings)
+    Static(C,Settings)
+
+    return {
+        Category = {
+            Name = "Bloxburg",
+            Title = "Bloxburg",
+            Image = nil, -- Set Image to nil in order to get game image!
+            Layout = 20,
+        },
+        Tab = {
+            {
+				Title = "Kick On Staff",
+				Tooltip = "Kicks you out of the game if a staff is detected in your game",
+				Layout = 0, Threads = {},
+				Shortcut = "KickOnStaff",Default=true, DontActivate=true,
+				Activate = function(self,newValue)
+                    if not newValue then
+                        return
+                    end
+                    for num, theirPlr in ipairs(Players:GetPlayers()) do
+                        if theirPlr ~= C.plr then
+                            self.Events.PlayerAdded(self,theirPlr,false)
+                        end
+                    end
+                end,
+                Events = {
+                    OthersPlayerAdded = function(self,theirPlr,firstRun)
+                        local tag
+                        while true do
+                            tag = theirPlr:GetAttribute("_tag")
+                            if tag then
+                                return
+                            else
+                                theirPlr:GetAttributeChangedSignal("_tag"):Wait()
+                            end
+                        end
+                        if tag < 0 or tag > 2 then
+                            C.plr:Kick(`[KickOnStaff]: You were kicked because {theirPlr.Name} is in the game and has the tag rank of {tag}`)
+                        end
+                    end
+                },
+            },
+        }
+    }
+end
