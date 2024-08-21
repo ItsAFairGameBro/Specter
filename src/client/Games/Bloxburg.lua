2b11fcbcdb4707964d435ce7531f722794aebec8
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-20 23:51:03 -0500

    update Bloxburg.lua

--- src/client/Games/Bloxburg.lua
+++ src/client/Games/Bloxburg.lua
@@ -30,7 +30,7 @@ return function (C,Settings)
                     local HotbarModule = C.StringWait(C.PlayerScripts,"Modules.HotbarUI")
                     table.insert(self.Functs,HotbarModule.ChildAdded:Connect(function(child)
                         if child:IsA("BindableEvent") then
-                            task.wait(.5)
+                            task.wait()
                             child:Fire()
                             print("FIRE2")
                         end
