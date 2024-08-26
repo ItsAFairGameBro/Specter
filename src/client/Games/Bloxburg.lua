a76b35a66a723d5c3e3fba14739996df4184053c
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-25 22:01:37 -0500

    update Bloxburg.lua

--- src/client/Games/Bloxburg.lua
+++ src/client/Games/Bloxburg.lua
@@ -374,7 +374,7 @@ return function (C,Settings)
                                         --end
                                     end
                                 end
-                                local IceCreamLoc = Vector3.new(937.986,13.7269,1043.946) -- Vector3.new(932,13.72,1047)
+                                local IceCreamLoc = Vector3.new(936.986,13.7269,1042.946) -- Vector3.new(932,13.72,1047)
                                 if (IceCreamLoc - C.char:GetPivot().Position).Magnitude > 1.2 then
                                     C.human:MoveTo(IceCreamLoc)
                                 end
