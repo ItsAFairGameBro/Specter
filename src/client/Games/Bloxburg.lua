2da8ff9d88a9c1e4167866de92e7417b64fd4d6f
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-08-27 03:22:24 -0500

    update Bloxburg.lua

--- src/client/Games/Bloxburg.lua
+++ src/client/Games/Bloxburg.lua
@@ -646,7 +646,7 @@ return function (C,Settings)
                             task.wait(1)
                         end
                         actionClone = actionClone or C.AddAction(info)
-                        while C.char and not C.IsBusy() and ((jobHandler:GetJob() ~= jobName and not botData.ProximityOnly)
+                        while C.char and not C.IsBusy() and ((jobHandler:GetJob() ~= jobName and not false)--botData.ProximityOnly)
                             or (botData.Location and (botData.Location.CFrame.Position - C.char:GetPivot().Position).Magnitude > 500)) do
                             if jobHandler:GetJob() then
                                 jobHandler:StopWorking()
