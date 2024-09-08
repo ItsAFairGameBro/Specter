d397dc5fbb7fa93c3d208bc7ee2e2897789648b3
ItsAFairGameBro <dcool6846@gmail.com>
2024-09-08 15:27:45 -0500

    update init.client.lua

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -482,7 +482,7 @@ function C.HookMethod(hook, name, runFunct, methods, source)
 		end
 		
 		local OriginFunct
-		CallFunction = function(self,...)
+		local function CallFunction(self,...)
 			-- Get the method being called
 			local method
 			if HookType=="hookmetamethod" then
