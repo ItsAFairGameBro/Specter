001805e29b0043dcec1bab4248abcb132d8cede1
ItsAFairGameBro <dcool6846@gmail.com>
2024-10-20 04:48:00 -0500

    update Commands.lua

--- src/client/Hacks/Commands.lua
+++ src/client/Hacks/Commands.lua
@@ -285,7 +285,7 @@ return function(C,Settings)
                 end,
             },
             Run=function(self,args)
-                local selectedName = (args[2] == "" and "no") or C.checkFriendsPCALLFunction(args[2])
+                local selectedName = (args[2] == "" and "no") or C.checkFriendsPCALLFunction(args[2])[1]
                 if not selectedName then
                     return false,`User Not Found: {args[2]}`--C.CreateSysMessage(`User Not Found: {args[2]}`)
                 end
@@ -313,7 +313,6 @@ return function(C,Settings)
                 else
                     args[3] = nil
                 end
-                print(outfitData, selectedName)
                 local defaultHumanDesc = selectedName~="no" and
                     (args[3] and PS:GetHumanoidDescriptionFromOutfitId(outfitData.id) or PS:GetHumanoidDescriptionFromUserId(selectedName.UserId))
                 if defaultHumanDesc == nil then
