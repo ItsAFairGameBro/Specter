a759327bdf908a130aff4b124fd76f466faa77de
ItsAFairGameBro <dcool6846@gmail.com>
2024-10-22 08:50:56 -0500

    update Blatant.lua

--- src/client/Hacks/Blatant.lua
+++ src/client/Hacks/Blatant.lua
@@ -420,7 +420,7 @@ return function(C,Settings)
 						return
 					end
                     if not newValue and C.human and C.human.FloorMaterial ~= Enum.Material.Air then
-                        C.human:ChangeState(Enum.HumanoidStateType.Landed)
+                        C.human:ChangeState(Enum.HumanoidStateType.Running)
                     end
 					if C.human then
 						if newValue and not self.EnTbl.EnClimbing then
