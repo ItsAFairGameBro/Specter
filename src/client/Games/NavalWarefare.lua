a011aeab20c0dc7ab513b95251d5e3ce50b02443
ItsAFairGameBro <dcool6846@gmail.com>
2024-10-31 18:49:41 -0500

    update NavalWarefare.lua

--- src/client/Games/NavalWarefare.lua
+++ src/client/Games/NavalWarefare.lua
@@ -207,7 +207,7 @@ local function Static(C,Settings)
 		return closestPart,closestDist
 	end
     table.insert(C.InsertCommandFunctions,function()
-        local LastBanVoteKick
+        C.getgenv().LastBanVoteKick = nil
         local LegitVoteKick = true
         local TimeNeeded = LegitVoteKick and 60.4 or 3
         return {
@@ -259,7 +259,7 @@ local function Static(C,Settings)
                             warn("Votekick",C.GetPlayerName(targetPlr),
                                 "Completed:",JustKicked and "BANNED" or "LEFT","after",targetPlr:GetAttribute("KickCounter") or 0,"Counters")
                             C.RemoveAction("NavalVotekick")
-                            LastBanVoteKick = targetPlr.DisplayName
+                            C.getgenv().LastBanVoteKick = targetPlr.DisplayName
                             C.CreateSysMessage(`Stopped banning because {C.GetPlayerName(targetPlr)} left. ` ..
                                 `It is {JustKicked and "HIGHLY LIKELY" or "POSSIBLE"} that they were banned!`
                                 .. ` (You voted {targetPlr:GetAttribute("KickCounter") or 0} times)`,
@@ -301,12 +301,13 @@ local function Static(C,Settings)
                 Parameters = {},
                 AfterTxt = " %s",
                 Run = function(self, args)
+                    local LastBanVoteKick = C.getgenv().LastBanVoteKick
                     if not LastBanVoteKick then
                         return false, "Nobody was recently votekicked!"
                     end
                     local parseMultiLine = getgenv().C.hackData.World.ChatEdit.ParseMultiLine
                     C.SendGeneralMessage(parseMultiLine(`\\n{LastBanVoteKick} was PERMANENTLY banned from this server!`));
-                    LastBanVoteKick = nil
+                    C.getgenv().LastBanVoteKick = nil
                 end,
             }
         }
