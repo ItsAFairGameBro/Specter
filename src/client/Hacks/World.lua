ce7ca65357793a3d7981784db6b1f4c70797a8e9
ItsAFairGameBro <dcool6846@gmail.com>
2024-10-31 18:11:49 -0500

    update World.lua

--- src/client/Hacks/World.lua
+++ src/client/Hacks/World.lua
@@ -631,8 +631,8 @@ return function(C_new,Settings)
 						--if rawget(C,"ChatVersion") == "TextChatService" and false then
 						--	newMessage ..= '\n' .. curMessage
 						--if rawget(C,"ChatVersion") == "LegacyChatService" then
-						newMessage ..= rawget(string,"sub")(rawget(string,"rep")(" ",155),#rawget(splitArray,num-1))..curMessage
-						--end
+						newMessage ..= rawget(string,"sub")(rawget(string,"rep")(" ",200),#rawget(splitArray,num-1))..curMessage
+						--end-- used to be 155
 					end
 					return newMessage
 				end,
