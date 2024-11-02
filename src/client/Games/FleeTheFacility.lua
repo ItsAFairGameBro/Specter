90a7363df36b1bf6feec5e09f99a309c8fd5bb11
ItsAFairGameBro <dcool6846@gmail.com>
2024-11-02 14:27:13 -0500

    update FleeTheFacility.lua

--- src/client/Games/FleeTheFacility.lua
+++ src/client/Games/FleeTheFacility.lua
@@ -995,7 +995,7 @@ return function(C,Settings)
                                 if not subject then
                                     return
                                 end
-                                local isInGame = not C.IsInBox(LobbyOBWall.CFrame, LobbyOBWall.Size, subject.Parent.Position, true)
+                                local isInGame = not C.IsInBox(LobbyOBWall.CFrame, LobbyOBWall.Size, subject.Parent:GetPivot().Position, true)
                                 if isInGame then
                                     local Ret = C.Map and FindChild(C.Map, "_LightingSettings") or nil
                                     return "Spoof", {Ret}
