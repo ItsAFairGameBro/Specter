1e944c42d653273136de77e5af28f985c72e63a3
ItsAFairGameBro <163351800+ItsAFairGameBro@users.noreply.github.com>
2024-12-01 00:44:34 -0600

    update FleeTheFacility.lua

--- src/client/Games/FleeTheFacility.lua
+++ src/client/Games/FleeTheFacility.lua
@@ -156,7 +156,7 @@ local function GetSharedHacks(C, Settings)
                 `Are you sure that you want to buy these hammers and gems?\nIf you are unsure, configure this under "Speed Buy".`, "Y/N")
             info.CanCancel -= 1
             if CanContinue then
-
+                C.hackData.FleeTheFacility.InstaTrade:SetValue(false) -- Disable the trading mechanism while we buy bundles!
                 for n, data in ipairs(ItemsToBuy) do
                     GetItemWhileNotLimit(table.unpack(data))
                 end
