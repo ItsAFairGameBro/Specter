ab5b7499118209bec192e363dd4ab15530ef6c99
ItsAFairGameBro <dcool6846@gmail.com>
2024-12-08 22:28:18 -0600

    update TowerBattles.lua

--- src/client/full_code.lua
+++ src/client/full_code.lua
@@ -6695,7 +6695,7 @@ return function(C,Settings)
                     end
                     local ScaleDif = Rig.PrimaryPart.Size.X / FakeRig.PrimaryPart.Size.X
                     FakeRig:ScaleTo(ScaleDif)
-                    FakeRig:PivotTo(zombie:GetPivot() + Vector3.new(0, C.getCharacterHeight(FakeRig)*1.5 - 1.4, 0))
+                    FakeRig:PivotTo(Rig:GetPivot())-- + Vector3.new(0, C.getCharacterHeight(FakeRig)*1.5 - 1.4, 0))
                     table.insert(self.Instances, FakeRig)
                     local RigWeld = Instance.new("WeldConstraint")
                     RigWeld.Part0 = Block--:FindFirstChild("FakeBlock")
