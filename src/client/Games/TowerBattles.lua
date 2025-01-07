9d9dc0a6bdd992c1ff87105002dd4d6949331e79
ItsAFairGameBro <dcool6846@gmail.com>
2025-01-07 16:30:27 -0600

    update TowerBattles.lua

--- src/client/Games/TowerBattles.lua
+++ src/client/Games/TowerBattles.lua
@@ -938,8 +938,8 @@ return function(C,Settings)
                     self:ApplyInvis(zombie, true)
                     local FakeRig = Players:CreateHumanoidModelFromDescription(Desc2Apply, Enum.HumanoidRigType.R6)
                     table.insert(self.Instances, FakeRig)
-                    C.PostApplyHumanoidDescription(FakeRig, Desc2Apply)
                     local FakeHuman = FakeRig:WaitForChild("Humanoid")
+                    C.PostApplyHumanoidDescription(FakeHuman, Desc2Apply)
                     FakeHuman.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
                     FakeHuman.HealthDisplayType = Enum.HumanoidHealthDisplayType.AlwaysOff
                     --FakeHuman:ChangeState(Enum.HumanoidStateType.Physics)
