0d5b988faf8efe16cddd6ed915e8e7a3fae94ceb
Acoolguy25 <dcool6846@gmail.com>
2025-02-08 01:18:20 -0600

    sync

--- src/client/full_code.lua
+++ src/client/full_code.lua
@@ -11498,7 +11498,7 @@ return function(C,Settings)
     local CheckIfValid
     -- Here's where the anti cheat stuff is done
     local AntiCheat = {
-        {
+        { -- ADONTIS ANTI CHEAT
             Run = function(self)
                 CheckIfValid = function()
                     local traceback = debug.traceback()
@@ -11539,7 +11539,7 @@ return function(C,Settings)
                 return true -- indicate that the anti cheat is successful
             end,
             KeepGoing = false, RunOnce = true,
-            GameIds = {1069466626,495693931},
+            GameIds = {1069466626,495693931,2719766208},
             PlaceIds = {},
         },
         {
@@ -11746,7 +11746,7 @@ return function(C,Settings)
                 end)
             end,
             KeepGoing = false, RunOnce = true,
-            GameIds = {3734304510,2719766208},PlaceIds={}, 
+            GameIds = {3734304510},PlaceIds={}, 
         },
         {
             Run = function(self)
