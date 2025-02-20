5cf3a3081ace6917fc671be5896bd78b4983396b
Acoolguy25 <dcool6846@gmail.com>
2025-02-19 18:56:26 -0600

    update CoreEnv.lua

--- src/client/Modules/CoreEnv.lua
+++ src/client/Modules/CoreEnv.lua
@@ -270,7 +270,6 @@ return function(C,Settings)
 			color = color or Color3.fromRGB(255,255,255);
 			local channel = TCS:FindFirstChild("RBXGeneral",true) or TCS:FindFirstChildWhichIsA("TextChannel",true)
 			message = (`%s <font color="rgb(%.0f,%.0f,%.0f)">%s</font>`):format(typeText, color.R*255, color.G*255, color.B*255, message)
-			C.setclipboard(message)
 			channel:DisplaySystemMessage(message, 'systemMessage');
 		else
 			SG:SetCore("ChatMakeSystemMessage",  { Text = `{typeText} {message}`, Color = color or Color3.fromRGB(255),
