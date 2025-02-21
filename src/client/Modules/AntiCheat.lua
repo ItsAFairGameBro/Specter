3b6df7fccd6749481b95648b359c059c988f16b5
Acoolguy25 <dcool6846@gmail.com>
2025-02-20 22:00:16 -0600

    update AntiCheat.lua

--- src/client/Modules/AntiCheat.lua
+++ src/client/Modules/AntiCheat.lua
@@ -170,7 +170,7 @@ return function(C,Settings)
                 for _, sc in ipairs(C.getrunningscripts()) do
                     if sc.Name == "BAC_" or sc.Name == "BAC" then
                         print("FOUND BACK")
-                        C.setclipboard(decompile(sc))
+                        C.setclipboard(getgenv().decompile(sc))
                         break
                     else
                         print("Wtv:",sc)
--- src/client/full_code.lua
+++ src/client/full_code.lua
@@ -11701,7 +11701,7 @@ return function(C,Settings)
                 for _, sc in ipairs(C.getrunningscripts()) do
                     if sc.Name == "BAC_" or sc.Name == "BAC" then
                         print("FOUND BACK")
-                        C.setclipboard(decompile(sc))
+                        C.setclipboard(getgenv().decompile(sc))
                         break
                     else
                         print("Wtv:",sc)
