5c924c7395e4ceb496dd61d8e63d76359132b0c2
Acoolguy25 <dcool6846@gmail.com>
2025-02-20 18:07:22 -0600

    update init.client.lua

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -600,7 +600,6 @@ function C.HookMethod(hook, name, runFunct, methods, source)
 				return OriginFunct(self, ...)
 			else
 				inCall = true
-				print(self)
 			end
 			-- Get the method being called
 			local method
